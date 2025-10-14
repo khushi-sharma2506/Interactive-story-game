25c965383b58d41ff9a29d39fa4ec2f94eec080f
Akhil-00001 <akhilkotnala397@gmail.com>
2025-10-14 19:49:41 +0530

    Delete src directory

--- src/graph.h
+++
@@ -1,6 +0,0 @@
-#include "structure.h"
-#define NUM_SCENES 24
-#define MAX_TEXT 2000
-scene* array[100];
-char arr[NUM_SCENES][MAX_TEXT];
-scene* create();
--- src/stack.h
+++
@@ -1,8 +0,0 @@
-#define MAX_STACK 100
-scene* Undo[MAX_STACK];
-scene* Redo[MAX_STACK];
-void run(scene*);
-scene* undo();
-scene* redo();
-void display();
-scene* gotosave(scene*);
